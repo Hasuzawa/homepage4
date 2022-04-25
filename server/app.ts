b149eea0849ae1f4c64cfdcebacc9f5752200615
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2022-04-25 20:51:47 +0900

    working typescript express server

---
+++ server/app.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import "dotenv"
+
+
+const PORT = process.env.PORT ?? 8080
+const HOST = process.env.HOST ?? "0.0.0.0"
+
+const app = express()
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+
+app.listen(PORT)
+console.log(`node.js server running on ${HOST}:${PORT}`)
