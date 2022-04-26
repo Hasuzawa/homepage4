3b14b5b29512c2b1d816b00015f6c41bdb7ba5d0
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2022-04-26 23:56:36 +0900

    Docker compose for both end

--- backend/app.ts
+++ backend/app.ts
@@ -2,7 +2,7 @@ import express from "express"
 import "dotenv"
 
 
-const PORT = process.env.PORT ?? 8080
+const PORT = process.env.PORT ?? 5000
 const HOST = process.env.HOST ?? "0.0.0.0"
 
 const app = express()
